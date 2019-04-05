7991030b74218b3c30525afe1c35095449aa9364
Eddie Chung <designered@gmail.com>
2019-04-05 12:53:39 +0200

    updated assets links

--- scripts/constants.js
+++ scripts/constants.js
@@ -4,78 +4,78 @@ const GAME_HEIGHT = 450;
 const BGCOLOR = 110;
 const GRAVITY = 0.35;
 let jump = -8;
-console.log("Constants.js loaded..");
+
 function preload() {
-  pushSound = loadSound("./assets/sounds/rolling.mp3");
-  ollieSound = loadSound("../assets/sounds/ollie.mp3");
-  hurtSound = loadSound("../assets/sounds/hurt.mp3");
-  hipHop1 = loadSound("../assets/sounds/oldschoolhiphop.wav");
-  hipHop2 = loadSound("../assets/sounds/hiphop.wav");
-  kaChing = loadSound("../assets/sounds/ka-ching.mp3");
+  pushSound = loadSound("assets/sounds/rolling.mp3");
+  ollieSound = loadSound("assets/sounds/ollie.mp3");
+  hurtSound = loadSound("assets/sounds/hurt.mp3");
+  hipHop1 = loadSound("assets/sounds/oldschoolhiphop.wav");
+  hipHop2 = loadSound("assets/sounds/hiphop.wav");
+  kaChing = loadSound("assets/sounds/ka-ching.mp3");
 
-  fontRegular = loadFont("../assets/fonts/Retro Gaming.ttf");
-  gameOver = loadImage("../assets/img/gameOver/gameOver.jpg");
-  city2 = loadImage("../assets/img/city1/City2.png");
-  city3 = loadImage("../assets/img/city1/City3.png");
-  city4 = loadImage("../assets/img/city1/City4.png");
-  road1 = loadImage("../assets/img/city1/road-border.png");
-  wheelsHydrant = loadImage("../assets/img/city1/wheels-hydrant.png");
-  boxesContainer = loadImage("../assets/img/city1/boxes-container.png");
-  buildings = loadImage("../assets/img/city1/buildings.png");
-  sky = loadImage("../assets/img/city1/sky.png");
-  wall1 = loadImage("../assets/img/city1/wall1.png");
-  wall2 = loadImage("../assets/img/city1/wall2.png");
+  fontRegular = loadFont("assets/fonts/Retro Gaming.ttf");
+  gameOver = loadImage("assets/img/gameOver/gameOver.jpg");
+  city2 = loadImage("assets/img/city1/City2.png");
+  city3 = loadImage("assets/img/city1/City3.png");
+  city4 = loadImage("assets/img/city1/City4.png");
+  road1 = loadImage("assets/img/city1/road-border.png");
+  wheelsHydrant = loadImage("assets/img/city1/wheels-hydrant.png");
+  boxesContainer = loadImage("assets/img/city1/boxes-container.png");
+  buildings = loadImage("assets/img/city1/buildings.png");
+  sky = loadImage("assets/img/city1/sky.png");
+  wall1 = loadImage("assets/img/city1/wall1.png");
+  wall2 = loadImage("assets/img/city1/wall2.png");
 
-  binCone = loadImage("../assets/img/obstacles/cropped/Mask Group cone.png");
-  groundCrack = loadImage("../assets/img/obstacles/cropped/Mask Group crack.png");
-  brickTile = loadImage("../assets/img/obstacles/brick.png");
-  cashMoney = loadImage("../assets/img/obstacles/cropped/Mask Group cash.png");
-  clock = loadImage("../assets/img/obstacles/cropped/Timer Sprite 27.svg");
-  blueBar = loadImage("../assets/img/obstacles/cropped/Mask Group blueBar.png");
+  binCone = loadImage("assets/img/obstacles/cropped/Mask Group cone.png");
+  groundCrack = loadImage("assets/img/obstacles/cropped/Mask Group crack.png");
+  brickTile = loadImage("assets/img/obstacles/brick.png");
+  cashMoney = loadImage("assets/img/obstacles/cropped/Mask Group cash.png");
+  clock = loadImage("assets/img/obstacles/cropped/Timer Sprite 27.svg");
+  blueBar = loadImage("assets/img/obstacles/cropped/Mask Group blueBar.png");
 
   playerPush = loadAnimation(
-    "../assets/img/skater/cropped/Mask Group 14.png",
-    "../assets/img/skater/cropped/Mask Group 14.png",
-    "../assets/img/skater/cropped/Mask Group 15.png",
-    "../assets/img/skater/cropped/Mask Group 16.png",
-    "../assets/img/skater/cropped/Mask Group 17.png",
-    "../assets/img/skater/cropped/Mask Group 18.png",
-    "../assets/img/skater/cropped/Mask Group 19.png",
-    "../assets/img/skater/cropped/Mask Group 14.png",
-    "../assets/img/skater/cropped/Mask Group 14.png",
-    "../assets/img/skater/cropped/Mask Group 14.png",
-    "../assets/img/skater/cropped/Mask Group 14.png"
+    "assets/img/skater/cropped/Mask Group 14.png",
+    "assets/img/skater/cropped/Mask Group 14.png",
+    "assets/img/skater/cropped/Mask Group 15.png",
+    "assets/img/skater/cropped/Mask Group 16.png",
+    "assets/img/skater/cropped/Mask Group 17.png",
+    "assets/img/skater/cropped/Mask Group 18.png",
+    "assets/img/skater/cropped/Mask Group 19.png",
+    "assets/img/skater/cropped/Mask Group 14.png",
+    "assets/img/skater/cropped/Mask Group 14.png",
+    "assets/img/skater/cropped/Mask Group 14.png",
+    "assets/img/skater/cropped/Mask Group 14.png"
   );
   playerOllie = loadAnimation(
-    "../assets/img/skater/cropped/Mask Group 4.png",
-    "../assets/img/skater/cropped/Mask Group 3.png",
-    "../assets/img/skater/cropped/Mask Group 2.png",
-    "../assets/img/skater/cropped/Mask Group 5.png",
-    "../assets/img/skater/cropped/Mask Group 7.png",
-    "../assets/img/skater/cropped/Mask Group 7.png",
-    "../assets/img/skater/cropped/Mask Group 6.png",
-    "../assets/img/skater/cropped/Mask Group 8.png",
-    "../assets/img/skater/cropped/Mask Group 9.png",
-    "../assets/img/skater/cropped/Mask Group 10.png"
+    "assets/img/skater/cropped/Mask Group 4.png",
+    "assets/img/skater/cropped/Mask Group 3.png",
+    "assets/img/skater/cropped/Mask Group 2.png",
+    "assets/img/skater/cropped/Mask Group 5.png",
+    "assets/img/skater/cropped/Mask Group 7.png",
+    "assets/img/skater/cropped/Mask Group 7.png",
+    "assets/img/skater/cropped/Mask Group 6.png",
+    "assets/img/skater/cropped/Mask Group 8.png",
+    "assets/img/skater/cropped/Mask Group 9.png",
+    "assets/img/skater/cropped/Mask Group 10.png"
   );
   playerFlip = loadAnimation(
-    "../assets/img/skater/cropped/kickflip/k1.png",
-    "../assets/img/skater/cropped/kickflip/k2.png",
-    "../assets/img/skater/cropped/kickflip/k3.png",
-    "../assets/img/skater/cropped/kickflip/k4.png",
-    "../assets/img/skater/cropped/kickflip/k4.png",
-    "../assets/img/skater/cropped/kickflip/k5.png",
-    "../assets/img/skater/cropped/kickflip/k5.png",
-    "../assets/img/skater/cropped/kickflip/k6.png",
-    "../assets/img/skater/cropped/kickflip/k7.png",
-    "../assets/img/skater/cropped/kickflip/k8.png",
-    "../assets/img/skater/cropped/kickflip/k9.png"
+    "assets/img/skater/cropped/kickflip/k1.png",
+    "assets/img/skater/cropped/kickflip/k2.png",
+    "assets/img/skater/cropped/kickflip/k3.png",
+    "assets/img/skater/cropped/kickflip/k4.png",
+    "assets/img/skater/cropped/kickflip/k4.png",
+    "assets/img/skater/cropped/kickflip/k5.png",
+    "assets/img/skater/cropped/kickflip/k5.png",
+    "assets/img/skater/cropped/kickflip/k6.png",
+    "assets/img/skater/cropped/kickflip/k7.png",
+    "assets/img/skater/cropped/kickflip/k8.png",
+    "assets/img/skater/cropped/kickflip/k9.png"
   );
 
   skullHead = loadAnimation(
-    "../assets/img/obstacles/skull/skull1.png",
-    "../assets/img/obstacles/skull/skull2.png",
-    "../assets/img/obstacles/skull/skull3.png",
-    "../assets/img/obstacles/skull/skull4.png"
+    "assets/img/obstacles/skull/skull1.png",
+    "assets/img/obstacles/skull/skull2.png",
+    "assets/img/obstacles/skull/skull3.png",
+    "assets/img/obstacles/skull/skull4.png"
   );
 }
